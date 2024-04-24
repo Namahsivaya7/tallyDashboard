c1169ea68eb80c0487ae47e2d1fe60ebfc5a6ca3
kureti Namah sivaya <kuretinamahsivaya7@gmail.com>
2024-04-24 12:52:16 +0530

    dashoard

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import TabbedPanel from "@/components/TabbedPanel";
+
+
+export default function Home() {
+  return <main>
+<TabbedPanel/>
+  </main>;
+}
---
+++ src/components/TabbedPanel.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React from "react";
+
+import { Tabs, type TabsProps } from "antd";
+import Home from "./Home";
+
+const onChange = (key: string) => {
+  console.log(key);
+};
+
+const items: TabsProps["items"] = [
+    {
+        key: "0",
+        label: "Home",
+        children:<Home/>,
+      },
+  {
+    key: "1",
+    label: "Goods Provider",
+    children: "list of goods providers",
+  },
+  {
+    key: "2",
+    label: "Bill Entry",
+    children: "Making bill ",
+  },
+  {
+    key: "3",
+    label: "Payment Entry",
+    children: "Payment Entry Details",
+  },
+  {
+    key: "4",
+    label: "Balance Sheet",
+    children: "Balance Sheet of a goods provider",
+  },
+];
+
+const TabbedPanel : React.FC = () => {
+    return(
+  <Tabs
+    defaultActiveKey="1"
+    items={items}
+    onChange={onChange}
+    tabPosition="left"
+  />
+)};
+
+export default TabbedPanel;
